b88a3e6168eeaa68f2b024bfb9d070a721447954
CatalinChira <catalin_chira05@yahoo.com>
2024-02-16 11:57:33 +0200

    Created the interface PhoneBehaviour and the class Phone

---
+++ src/main/java/week4/phones/Phone.java
@@ -0,0 +1,7 @@
+package week4.phones;
+
+public abstract class Phone implements PhoneBehaviour {
+    private String color;
+    private String material;
+
+}
---
+++ src/main/java/week4/phones/PhoneBehaviour.java
@@ -0,0 +1,15 @@
+package week4.phones;
+
+public interface PhoneBehaviour {
+
+    public void createNewContact();
+
+    public void seeExistingContact();
+
+    public void sendTextMessage();
+
+    public void seeMessagesForContact();
+
+    public void makeACall();
+    public void seeAllCallHistory();
+}
