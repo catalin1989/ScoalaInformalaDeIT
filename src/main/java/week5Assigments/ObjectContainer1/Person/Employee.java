6bf9dc99795a38aa3111ceecd3004bd31ec49c6e
CatalinChira <catalin_chira05@yahoo.com>
2024-02-25 22:34:36 +0200

    Finished the homework

---
+++ src/main/java/week5Assigments/ObjectContainer1/Person/Employee.java
@@ -0,0 +1,30 @@
+package week5Assigments.ObjectContainer1.Person;
+
+import week5Assigments.ObjectContainer1.UtilityClases.Hobby;
+
+import java.util.List;
+
+public class Employee extends Person {
+    private final String status = "employee";
+
+    public Employee() {
+    }
+
+    public Employee(String name, int age) {
+        super(name, age);
+    }
+
+    public Employee(String name, int age, List<Hobby> hobbies) {
+        super(name, age, hobbies);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "status='" + status + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", hobbies=" + hobbies +
+                '}';
+    }
+}
---
+++ src/main/java/week5Assigments/ObjectContainer1/Person/Student.java
@@ -0,0 +1,30 @@
+package week5Assigments.ObjectContainer1.Person;
+
+import week5Assigments.ObjectContainer1.UtilityClases.Hobby;
+
+import java.util.List;
+
+public class Student extends Person {
+    private final String status = "student";
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, List<Hobby> hobbies) {
+        super(name, age, hobbies);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "status='" + status + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", hobbies=" + hobbies +
+                '}';
+    }
+}
