7908b3d45dddcc8d6008f13ebd85ea0ffd8ba77f
CatalinChira <catalin_chira05@yahoo.com>
2024-03-01 22:46:15 +0200

    Added the comments to the code.It works well.

--- src/main/java/week6TestingAssigment/Calculator.java
+++ src/main/java/week6TestingAssigment/Calculator.java
@@ -1,146 +1,170 @@
 package week6TestingAssigment;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 
+//This is the class where all the logic of the calculator is implemented
 public class Calculator {
-    Convertor convertor=new Convertor();
-    private Reader reader=new Reader();
-    private final int mm=1,cm=2,dm=3,m=4,km=5;
-    private List<String> mathematicalSigns=new ArrayList<>();
-    private List<String> numbersAndUnitMesurements=new ArrayList<>();
-    private List<Integer> convertedValues=new ArrayList<>();
+    Convertor convertor;
+    private Reader reader;
+    // private final int mm=1,cm=2,dm=3,m=4,km=5;
+    //in this field I will store the mathematical signs
+    private final List<String> mathematicalSigns = new ArrayList<>();
+    //in this field I will store all the values imputed from the user ex: 10 km;
+    private final List<String> numbersAndUnitMesurements = new ArrayList<>();
+    //in this field I will store the converted values 10 km converted to m, it will store 1000
+    private final List<Integer> convertedValues = new ArrayList<>();
     private int minimMeasurmentUnit;
-    private boolean errorFlag=false;
-    public Calculator(Reader reader,Convertor convertor){
-        this.reader=reader;
-        this.convertor=convertor;
+    private boolean errorFlag = false;
+
+    public Calculator(Reader reader, Convertor convertor) {
+        this.reader = reader;
+        this.convertor = convertor;
     }
 
-    private void assignTheInputData(){
-        for(int i=0;i<reader.getValues().size();i+=2){
+    //This method wil assign the values from the reader. I know that on even positions I have a number and
+    // on the even position I have a mathematical sign;
+    private void assignTheInputData() {
+        for (int i = 0; i < reader.getValues().size(); i += 2) {
             numbersAndUnitMesurements.add(reader.getValues().get(i));
         }
-        for(int i=1;i<reader.getValues().size();i+=2){
+        for (int i = 1; i < reader.getValues().size(); i += 2) {
             mathematicalSigns.add(reader.getValues().get(i));
         }
     }
-    private int determineTheMinimumUnitMeasurment(){
-        minimMeasurmentUnit=5;
-        List<Integer> valuesOfUnitMeasurment=new ArrayList<>();
-        for(String s:numbersAndUnitMesurements){
-            String[]array=s.split(" ");
-            switch (array[1]){
-                case "km":valuesOfUnitMeasurment.add(5);
-                break;
-                case "m":valuesOfUnitMeasurment.add(4);
-                break;
-                case "dm":valuesOfUnitMeasurment.add(3);
-                break;
-                case "cm":valuesOfUnitMeasurment.add(2);
-                break;
-                case "mm":valuesOfUnitMeasurment.add(1);
-                break;
+
+    //This method will determine the minimum unit measurement. I have assigned a number to the unit measurement
+    // mm=1,cm=2,dm=3,m=4,km=5;
+    //This will check all the unit measurements in the numbersAndUnitMesurements and will input a number into valuesOfUnitMeasurment
+    //Then it will compare the values and return the minimum number
+    private int determineTheMinimumUnitMeasurment() {
+        minimMeasurmentUnit = 5;
+        List<Integer> valuesOfUnitMeasurment = new ArrayList<>();
+        for (String s : numbersAndUnitMesurements) {
+            String[] array = s.split(" ");
+            switch (array[1]) {
+                case "km":
+                    valuesOfUnitMeasurment.add(5);
+                    break;
+                case "m":
+                    valuesOfUnitMeasurment.add(4);
+                    break;
+                case "dm":
+                    valuesOfUnitMeasurment.add(3);
+                    break;
+                case "cm":
+                    valuesOfUnitMeasurment.add(2);
+                    break;
+                case "mm":
+                    valuesOfUnitMeasurment.add(1);
+                    break;
             }
-           for(int i:valuesOfUnitMeasurment){
-               if(i<minimMeasurmentUnit){
-                   minimMeasurmentUnit=i;
-               }
-           }
+            for (int i : valuesOfUnitMeasurment) {
+                if (i < minimMeasurmentUnit) {
+                    minimMeasurmentUnit = i;
+                }
+            }
         }
         return minimMeasurmentUnit;
     }
+    //This method, based on the determineTheMinimumUnitMeasurment, will convert all the numbers to the minimum value
 
-    private void convertInputValues(){
-        int minim=determineTheMinimumUnitMeasurment();
-        switch (minim){
-            case 5:for(String s:numbersAndUnitMesurements){
-                String[] array=s.split(" ");
-                int number=Integer.parseInt(array[0]);
-                convertedValues.add(number);
-            }
-            break;
-            case 4:for(String s:numbersAndUnitMesurements){
-                String[] array=s.split(" ");
-                int number=convertor.convertToMeters(array[1],Integer.parseInt(array[0]));
-                convertedValues.add(number);
-            }
-            break;
-            case 3:for(String s:numbersAndUnitMesurements){
-                String[] array=s.split(" ");
-                int number=convertor.convertToDecimeters(array[1],Integer.parseInt(array[0]));
-                convertedValues.add(number);
-            }
-            break;
-            case 2:for(String s:numbersAndUnitMesurements){
-                String[] array=s.split(" ");
-                int number=convertor.convertToCentimeters(array[1],Integer.parseInt(array[0]));
-                convertedValues.add(number);
-            }
-            break;
-            case 1:for(String s:numbersAndUnitMesurements){
-                String[] array=s.split(" ");
-                int number=convertor.convertToMilimeters(array[1],Integer.parseInt(array[0]));
-                convertedValues.add(number);
-            }
+    private void convertInputValues() {
+        int minim = determineTheMinimumUnitMeasurment();
+        switch (minim) {
+            case 5:
+                for (String s : numbersAndUnitMesurements) {
+                    String[] array = s.split(" ");
+                    int number = Integer.parseInt(array[0]);
+                    convertedValues.add(number);
+                }
+                break;
+            case 4:
+                for (String s : numbersAndUnitMesurements) {
+                    String[] array = s.split(" ");
+                    int number = convertor.convertToMeters(array[1], Integer.parseInt(array[0]));
+                    convertedValues.add(number);
+                }
+                break;
+            case 3:
+                for (String s : numbersAndUnitMesurements) {
+                    String[] array = s.split(" ");
+                    int number = convertor.convertToDecimeters(array[1], Integer.parseInt(array[0]));
+                    convertedValues.add(number);
+                }
+                break;
+            case 2:
+                for (String s : numbersAndUnitMesurements) {
+                    String[] array = s.split(" ");
+                    int number = convertor.convertToCentimeters(array[1], Integer.parseInt(array[0]));
+                    convertedValues.add(number);
+                }
+                break;
+            case 1:
+                for (String s : numbersAndUnitMesurements) {
+                    String[] array = s.split(" ");
+                    int number = convertor.convertToMilimeters(array[1], Integer.parseInt(array[0]));
+                    convertedValues.add(number);
+                }
         }
     }
-    private int calculateTheResult(){
-        int result=0;
-        try{
-            if(convertedValues.size()==0){
-                errorFlag=true;
+
+    //This method will do all the math
+    //It assigns the first value to the result, then it goes it checks the mathematical sign in the mathematicalSigns
+    //and performs the requested operation
+    private int calculateTheResult() {
+        int result = 0;
+        try {
+            if (convertedValues.isEmpty()) {
+                errorFlag = true;
                 throw new IllegalArgumentException("No numbers to process");
 
             }
-         result=convertedValues.get(0);
-        for(int i=0;i<convertedValues.size()-1;i++){
-            if(mathematicalSigns.get(i).equals("+")){
-                result+=convertedValues.get(i+1);
+            result = convertedValues.get(0);
+            for (int i = 0; i < convertedValues.size() - 1; i++) {
+                if (mathematicalSigns.get(i).equals("+")) {
+                    result += convertedValues.get(i + 1);
+                } else {
+                    result -= convertedValues.get(i + 1);
+                }
             }
-            else{
-                result-=convertedValues.get(i+1);
-            }
-        }
-        return result;
-    }catch(Exception e){
+            return result;
+        } catch (Exception e) {
             System.out.println("Error");
         }
         return result;
     }
-    public void printTheResult(){
+
+    //I wanted to encapsulate all the important methods and give acces to the user only to the printTheResult method
+    //This method calls all the important methods of the other objects and the own methods and prints the resul.
+    public void printTheResult() throws IOException {
+        reader.read();
         assignTheInputData();
         convertInputValues();
-        int result=calculateTheResult();
-        if(errorFlag){
+        int result = calculateTheResult();
+        if (errorFlag) {
             System.out.println("The calculator can't print the result due to an error");
-                return;
+            return;
         }
-        switch (minimMeasurmentUnit){
+        switch (minimMeasurmentUnit) {
             case 5:
-                System.out.println("The result of the operation is: "+result+" km.");
+                System.out.println("The result of the operation is: " + result + " km.");
                 break;
             case 4:
-                System.out.println("The result of the operation is: "+result+" m.");
+                System.out.println("The result of the operation is: " + result + " m.");
                 break;
             case 3:
-                System.out.println("The result of the operation is: "+result+" dm.");
+                System.out.println("The result of the operation is: " + result + " dm.");
                 break;
             case 2:
-                System.out.println("The result of the operation is: "+result+" cm.");
+                System.out.println("The result of the operation is: " + result + " cm.");
                 break;
             case 1:
-                System.out.println("The result of the operation is: "+result+" mm.");
+                System.out.println("The result of the operation is: " + result + " mm.");
                 break;
         }
     }
-
-
-
 
 
     public Reader getReader() {
--- src/main/java/week6TestingAssigment/Reader.java
+++ src/main/java/week6TestingAssigment/Reader.java
@@ -6,45 +6,68 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+//This class is responsible for the input from the user
 public class Reader {
-   private List<String> values=new ArrayList<>();
+    //It stores the values from the user in an ArrayList
+    private List<String> values = new ArrayList<>();
 
 
     public List<String> getValues() {
         return values;
     }
 
-    public void read() throws IOException {
+    //This method uses a try-with-resources to automatically close the BufferedReader at the end
+    //It accepts a line with a value ex: 10 km and the nex line a mathematical sign like + - =
+    //For the values I have protected the method by not allowing the user to enter an empty field,
+    //a negative number, a number without a unit measurement, and an unknown unit measurement. If the user tries
+    //to input the above examples, the program will stop and it will show us the reason why it has stopped.
+    //For the mathematical signs, if the user tries to enter something else, they will get an error with a message.
+    protected void read() throws IOException {
 
-        try(BufferedReader reader=new BufferedReader(new InputStreamReader(System.in))){
-            while(true){
-                String data=reader.readLine();
-                String[] array=data.split(" ");
-                try{
-                    int number=Integer.parseInt(array[0]);
-                    if(array[1]==null){
-                        throw new Exception();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            while (true) {
+                String data = reader.readLine();
+                String[] array = data.split(" ");
+                try {
+                    if (array.length == 1) {
+                        throw new IllegalArgumentException("You entered an empty line!");
                     }
-                }
-                catch(Exception e){
-                    System.out.println("You haven't entered a valid number and/or unit measurement.Please rerun the program");
-                    break;
-                }
-                if (!array[1].equals("mm") && !array[1].equals("cm") && !array[1].equals("dm")&&!array[1].equals("m")&&!array[1].equals("km")) {
-                    System.out.println("Unknown unit measurement format!");
-                }
-                values.add(data);
-                String sign= reader.readLine();
-                if(!sign.equals("+")&&!(sign.equals("-"))&&!sign.equals("=")){
-                    System.out.println("Unknown operation type.Please rerun the program");
-                    break;
-                }
-                if(sign.equals("=")){
+                    int number = Integer.parseInt(array[0]);
+                    if (number < 0) {
+                        throw new IllegalArgumentException("You entered a negative number. A distance can't be negative!");
+                    }
+                    if (array[1] == null) {
+                        throw new IllegalArgumentException("You haven't entered a unit measurement!");
+                    }
+                    if (!array[1].equals("mm") && !array[1].equals("cm") && !array[1].equals("dm") && !array[1].equals("m") && !array[1].equals("km")) {
+                        throw new IllegalArgumentException("Unknown unit measurement format!");
 
+                    }
+                    values.add(data);
+
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
                     break;
                 }
 
-                values.add(sign);
+                //If the user inputs an unknown mathematical sign, the method will clean the values ArrayList.
+                String sign = reader.readLine();
+                try {
+                    if (!sign.equals("+") && !(sign.equals("-")) && !sign.equals("=")) {
+                        values.clear();
+                        throw new IllegalArgumentException("Unknown operation type.Please rerun the program");
+
+                    }
+                    if (sign.equals("=")) {
+                        break;
+                    }
+                    values.add(sign);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    break;
+                }
+
+
             }
         }
 
