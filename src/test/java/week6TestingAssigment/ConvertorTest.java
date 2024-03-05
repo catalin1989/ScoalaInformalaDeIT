f29d0e6ae4920e24fd686d3353e5ebd1de69ef2a
CatalinChira <catalin_chira05@yahoo.com>
2024-03-05 22:28:25 +0200

    Tried some new stuff

--- src/test/java/week6TestingAssigment/ConvertorTest.java
+++ src/test/java/week6TestingAssigment/ConvertorTest.java
@@ -13,6 +13,7 @@ class ConvertorTest {
         convertor = new Convertor();
     }
 
+
     @Test
     void convertToMeters_10KM_successfully() {
         int number = convertor.convertToMeters("km", 10);
@@ -24,4 +25,5 @@ class ConvertorTest {
         int number = convertor.convertToDecimeters("km", 1);
         assertEquals(10000, number);
     }
+
 }
