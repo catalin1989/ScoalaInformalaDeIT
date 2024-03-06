501223ef951572f563b32dd38ab3a8b208bbe00e
CatalinChira <catalin_chira05@yahoo.com>
2024-03-06 12:09:51 +0200

    refined a method

--- src/test/java/week6TestingAssigment/ReaderTest.java
+++ src/test/java/week6TestingAssigment/ReaderTest.java
@@ -3,7 +3,11 @@ package week6TestingAssigment;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -44,5 +48,6 @@ class ReaderTest {
     void enteredValidMetricMeasurementUnits(String input){
         assertFalse(reader.enteredUnknownUnitMeasurement(input));
     }
+
 
 }
