97c070ee2eaf1660d5bf092f6b93a4a8b69f8970
CatalinChira <catalin_chira05@yahoo.com>
2024-04-03 11:10:33 +0300

    Implemented the logger functionality

--- src/main/java/week_8_exeptions_and_logging_assigment/Student.java
+++ src/main/java/week_8_exeptions_and_logging_assigment/Student.java
@@ -25,122 +25,126 @@ public class Student {
         this.sex = sex;
         validateCNP(CNP);
         this.CNP = CNP;
-        System.out.println("All the data entered for "+firstName+" "+lastName+" are valid. The student info can be added to the repository");
+        MyLogger.logger.info("All the data entered for " + firstName + " " + lastName + " are valid. The student info can be added to the repository");
 
     }
 
-    private void validateName(String s){
-        if (s==null){
+    private void validateName(String s) {
+        if (s == null) {
             throw new IllegalArgumentException("The name should not be null. Please enter a valid name!");
         }
-        if (s.isEmpty()){
+        if (s.isEmpty()) {
             throw new IllegalArgumentException("The name should not be empty. Please enter a valid name!");
         }
-        if (s.isBlank()){
+        if (s.isBlank()) {
             throw new IllegalArgumentException("The name should not be blank. Please enter a valid name!");
         }
     }
-    private void validateDateOfBirth(String dateOfBirth){
 
-        String[] array=dateOfBirth.split("\\.");
-        if(array.length!=3){
+    private void validateDateOfBirth(String dateOfBirth) {
+
+        String[] array = dateOfBirth.split("\\.");
+        if (array.length != 3) {
             throw new IllegalArgumentException("You have entered an invalid date. Please enter a valid number in the date field!");
         }
         try {
             int day = Integer.parseInt(array[0]);
-            int month=Integer.parseInt(array[1]);
-            int year=Integer.parseInt(array[2]);
-            if(day<1){
+            int month = Integer.parseInt(array[1]);
+            int year = Integer.parseInt(array[2]);
+            if (day < 1) {
                 throw new IllegalArgumentException("You can't have a negative number as a day. Please enter a valid number!");
             }
-            if(monthIsOdd(month)){
-                if(day>31){
+            if (monthIsOdd(month)) {
+                if (day > 31) {
                     throw new IllegalArgumentException("You can't have a day number over 31 in an odd month.Please enter a valid number!");
                 }
 
-                }  else{
-                if(day>30){
+            } else {
+                if (day > 30) {
                     throw new IllegalArgumentException("You can't have a day number over 30 in an even month. Please enter a valid number!");
                 }
             }
 
-            if(month==2){
-                if(yearIsLeap(year)&&day>29){
-                        throw new IllegalArgumentException("You can't have a day number over 29, on the second moth of a leap year. Please enter a valid number");
-                }
-                    else {
-                        if(!yearIsLeap(year)&&day>28){
-                            throw new IllegalArgumentException("You can't have a day number over 28, on the second month. Please enter a valid number.");
-                        }
+            if (month == 2) {
+                if (yearIsLeap(year) && day > 29) {
+                    throw new IllegalArgumentException("You can't have a day number over 29, on the second moth of a leap year. Please enter a valid number");
+                } else {
+                    if (!yearIsLeap(year) && day > 28) {
+                        throw new IllegalArgumentException("You can't have a day number over 28, on the second month. Please enter a valid number.");
+                    }
 
 
                 }
 
             }
-            if(month<1){
+            if (month < 1) {
                 throw new IllegalArgumentException("You can't have a negative number as a month. Please enter a valid number!");
             }
-            if(year<1900||year>(currentYear()-18)){
+            if (year < 1900 || year > (currentYear() - 18)) {
                 throw new IllegalArgumentException("You have entered an invalid number for a year! PLease enter a valid number");
             }
 
-        }
-        catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("You have entered an invalid date. Please enter a valid number in the date field");
         }
     }
-    private boolean monthIsOdd(int month){
-        boolean result=true;
-        if(month==2||month==4||month==6||month==8||month==10||month==12){
-            result=false;
+
+    private boolean monthIsOdd(int month) {
+        boolean result = true;
+        if (month == 2 || month == 4 || month == 6 || month == 8 || month == 10 || month == 12) {
+            result = false;
         }
         return result;
     }
-    private boolean yearIsLeap(int year){
-        boolean result=true;
-        if(year%4!=0){
-            result=false;
+
+    private boolean yearIsLeap(int year) {
+        boolean result = true;
+        if (year % 4 != 0) {
+            result = false;
         }
         return result;
     }
-    private int currentYear(){
-        Date date=new Date();
-        SimpleDateFormat format=new SimpleDateFormat("yyyy");
-        String year= format.format(date);
+
+    private int currentYear() {
+        Date date = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy");
+        String year = format.format(date);
         return Integer.parseInt(year);
     }
 
-    private void validateGender(char sex){
-        String s="MmFf";
-        if(s.indexOf(sex)==-1){
+    private void validateGender(char sex) {
+        String s = "MmFf";
+        if (s.indexOf(sex) == -1) {
             throw new IllegalArgumentException("You haven't entered a valid option for the sex of the student");
         }
     }
-    private void validateCNP(String cnp){
-        if(cnp.length()!=13){
+
+    private void validateCNP(String cnp) {
+        if (cnp.length() != 13) {
             throw new IllegalArgumentException("You haven't entered a full cnp. A CNP should have 13 numbers ");
         }
-        validateDateOfBirthWithCNP(cnp,dateOfBirth);
+        validateDateOfBirthWithCNP(cnp, dateOfBirth);
 
     }
-    private void validateDateOfBirthWithCNP(String cnp,String dateOfBirth){
-        String dayFromCNP=cnp.substring(5,7);
-        String monthFromCNP=cnp.substring(3,5);
-     String yearFromCNP=cnp.substring(1,3);
 
-     String dayFromEnteredBirthDay=dateOfBirth.substring(0,2);
-     String mothFromEnteredBirthDay=dateOfBirth.substring(3,5);
-     String yearFromEnteredBirthDay=dateOfBirth.substring(8);
+    private void validateDateOfBirthWithCNP(String cnp, String dateOfBirth) {
+        String dayFromCNP = cnp.substring(5, 7);
+        String monthFromCNP = cnp.substring(3, 5);
+        String yearFromCNP = cnp.substring(1, 3);
 
-     if(!dayFromCNP.equals(dayFromEnteredBirthDay)){
-         throw new IllegalArgumentException("You have a problem with the CNP and the entered birth day. The day does not match");
-     }
-     if(!monthFromCNP.equals(mothFromEnteredBirthDay)){
-         throw new IllegalArgumentException("You have a problem with the CNP and the entered birth day. The month does not match");
-     }
-     if(!yearFromCNP.equals(yearFromEnteredBirthDay)){
-         throw new IllegalArgumentException("You have a problem with the CNP and the entered birth day. The year does not match");
-     }
+        String dayFromEnteredBirthDay = dateOfBirth.substring(0, 2);
+        String mothFromEnteredBirthDay = dateOfBirth.substring(3, 5);
+        String yearFromEnteredBirthDay = dateOfBirth.substring(8);
+
+        if (!dayFromCNP.equals(dayFromEnteredBirthDay)) {
+            throw new IllegalArgumentException("You have a problem with the CNP and the entered birth day. The day does not match");
+        }
+        if (!monthFromCNP.equals(mothFromEnteredBirthDay)) {
+            throw new IllegalArgumentException("You have a problem with the CNP and the entered birth day. The month does not match");
+        }
+        if (!yearFromCNP.equals(yearFromEnteredBirthDay)) {
+            throw new IllegalArgumentException("You have a problem with the CNP and the entered birth day. The year does not match");
+        }
 
     }
 
@@ -163,7 +167,7 @@ public class Student {
                 "first name='" + firstName + '\'' +
                 ", last name='" + lastName + '\'' +
                 ", birth day='" + dateOfBirth + '\'' +
-                ", CNP='" + CNP + '\''+
+                ", CNP='" + CNP + '\'' +
                 '}';
     }
 
